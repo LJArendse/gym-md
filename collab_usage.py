d55231e92bb8f0247ec33eb8ae358ad38dca62a3
LJArendse <lindsayjohnarendse@gmail.com>
2022-06-29 20:44:54 +0200

    Uplaod latest

--- collab_usage.py
+++ collab_usage.py
@@ -7,8 +7,15 @@ env = gym.make('md-collab-gene_1-v0')
 
 # Currently the only md-collab-simple-v0 env supports the dual action_type option
 action_type = 'path'
-action_type = 'directional'
+#action_type = 'directional'
 env = gym.make('md-collab-simple-v0', action_type=action_type)
+env = gym.make('md-collab-holmgard_1-v0', action_type=action_type)
+
+#screenshot
+# observation = env.reset()
+# output_image = env.render(mode='human')
+# output_image.save(f"md-collab-holmgard_1-v0_step0.jpg", quality='web_maximum')
+
 
 LOOP: int = 1000
 TRY_OUT: int = 1
@@ -17,7 +24,7 @@ for _ in range(TRY_OUT):
     observation = env.reset()
     reward_sum = 0
     for i in range(LOOP):
-        env.render(mode='human')
+        output_image = env.render(mode='human')
         actions = [random.random() for _ in range(7)]
         actions = [0.98,0.0,1.0,0.99,0.0,0.0,0.95]
         c_1actions = [0.98,0.0,1.0,0.99,0.0,0.0,0.95]
